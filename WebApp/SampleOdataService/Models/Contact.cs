3f7ef39c5530f13c90c89ffdc56643b5628a55d4
Juan Paez <juanpaez@microsoft.com>
2022-05-03 12:34:15 -0700

    add repro enum

--- WebApp/SampleOdataService/Models/Contact.cs
+++ WebApp/SampleOdataService/Models/Contact.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using TicketDataService.Models;
 
 namespace TicketDataService.Models
 {
+	
 	public class Contact
 	{
+		public enum GenderEnum
+		{
+			Male = 0,
+			Female = 1,
+			Other = 2
+		}
+
 		[Key]
 		public Guid ContactID { get; set; }
 		public string name { get; set; }
 		public int age { get; set; }
 
 		public double annualrevenue { get; set; }
+
+		public GenderEnum gender { get; set; }
+
 	}
 }
